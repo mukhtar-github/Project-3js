62235e1e60859f581fa2401ec639dd18b0fa9e7d
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-17 04:10:22 +0100

    Files updated

--- build/src/main.js
+++ build/src/main.js
@@ -1,10 +1,35 @@
 import {
-    Camera,
-    Material,
-    Texture,
-  } from './build/three.module.js';
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from './build/three.module.js';
 
-import { OrbitControls } from './examples/jsm/controls/OrbitControls.js';
+// Get a reference to the container element that will hold our scene
+const container = document.querySelector('#scene-container');
+
+// create a Scene
+const scene = new Scene();
+
+// Set the background color
+scene.background = new Color('skyblue');
+
+// Create a Camera
+const fov = 35; // AKA Field of View
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+
+
+
+
+
+
 
 
 
