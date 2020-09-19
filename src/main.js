66a9ed00f716ccf2eb52831355122f1a8ce70699
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-19 09:35:35 +0100

    Files updated

--- src/main.js
+++ src/main.js
@@ -47,6 +47,14 @@ const renderer = new WebGLRenderer();
 // next, set the renderer to the same size as our container element
 renderer.setSize(container.clientWidth, container.clientHeight);
 
+// finally, set the pixel ratio so that our scene will look good on retina displays
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// add the automatically created <canvas> element to the page
+container.append(renderer.domElement);
+
+// render, or 'create a still image', of the scene
+renderer.render(scene, camera);
 
 
 
