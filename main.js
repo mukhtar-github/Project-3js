f85806884adb24fb1ac965efbda0ed5309f758d0
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-22 17:31:52 +0100

    Files updated

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 import { World } from './World.js';
 
 function main() {
-  // code to set up the World App will go here
+  // create a new World instance
+  const world = new World();
+  
+  // Get a reference to the container element
+  const container = document.querySelector('#scene-container');
+
+  // add the canvas to the container
+  container.append(World.canvas);
+
+  // set the world size
+  world.setSize(
+    container.clientHeight,
+    container.clientWidth,
+    Window.devicePixelRatio
+  );
+
+  // draw the scene
+  world.render();
 }
 
 main();
