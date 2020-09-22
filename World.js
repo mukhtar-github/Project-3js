d86c96db6685028901d1353820b7d2a6c859125f
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-22 17:07:19 +0100

    Files updated

---
+++ World.js
@@ -0,0 +1,15 @@
+class World {
+    constructor() {
+        this.canvas = null; // TODO
+    }
+
+    render() {
+        // TODO
+    }
+
+    setSize(width, height, pixelRatio) {
+        // TODO
+    }
+}
+
+export { World };
