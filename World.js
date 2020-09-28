b7578b8eb3f5643b5168036aa8ff1e7e7c844632
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-28 08:53:14 +0100

    Files updated

--- World.js
+++ World.js
@@ -1,6 +1,14 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
 class World {
     constructor() {
         this.canvas = null; // TODO
+        this.camera = createCamera();
+        this.scene = createScene();
+        this.renderer = createRenderer();
     }
 
     render() {
--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import { World } from './World.js';
 function main() {
   // create a new World instance
   const world = new World();
+
+  // We can access member variables from the instance
+  console.log(world.camera);
+  console.log(world.renderer);
+  console.log(world.scene);
   
   // Get a reference to the container element
   const container = document.querySelector('#scene-container');
