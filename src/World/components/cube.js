4b81ea4010d9eba6e037cabe04a31086e4344b19
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-18 04:18:11 +0100

    Files updated

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,14 +1,16 @@
-import { BoxBufferGeometry, Mesh, MeshBasicMaterial } from 'https://unpkg.com/three@0.117.0/build/three.module.js';
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from 'https://unpkg.com/three@0.117.0/build/three.module.js';
 
 function createCube() {
   // create a geometry
   const geometry = new BoxBufferGeometry(2, 2, 2);
 
-  // create a default (white) Basic material
-  const material = new MeshBasicMaterial();
+  // a physically correct "standard" material
+  const material = new MeshStandardMaterial({ color: 'purple' });
 
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
+
+  cube.rotation.set(-0.5, -0.1, 0.8);
 
   return cube;
 }
