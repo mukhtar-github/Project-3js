5e695d18106573b2effd2130d66d8f336b70cebe
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-14 06:37:53 +0100

    Set up the event listener

--- src/World/systems/Resizer.js
+++ src/World/systems/Resizer.js
@@ -10,6 +10,11 @@ class Resizer {
   constructor(container, camera, renderer) {
     // set initial size on load
     setSize(container, camera, renderer);
+
+    window.addEventListener('resize', () => {
+      // set the size again if a resize occurs
+      setSize(container, camera, renderer);
+    });
   }
 }
 
