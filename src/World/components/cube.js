c42cc86911ce95510316bf09831863356cb7859b
Mukhtar <mukhtartanimugarba@gmail.com>
2020-12-14 06:48:29 +0100

    cube.js: create the .tick method

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -2,14 +2,18 @@ import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from 'https://unpkg.com
 
 function createCube() {
   const geometry = new BoxBufferGeometry(2, 2, 2);
-
-  // Switch the old "basic" material to
-  // a physically correct "standard" material
   const material = new MeshStandardMaterial({ color: 'purple' });
-
   const cube = new Mesh(geometry, material);
 
   cube.rotation.set(-0.5, -0.1, 0.8);
+
+  // this method will be called once per frame
+  cube.tick = () => {
+    // increase the cube's rotation each frame
+    cube.rotation.z += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  };
 
   return cube;
 }
